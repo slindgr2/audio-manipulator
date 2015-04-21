1c233cfb139a9eae31617e40c1a27a673b0b953f
slindgr2 <slindgr2@nebrwesleyan.edu>
2015-04-21 15:54:15 -0500

    Getting our amplitude, and frequency, lowering the amplitude when needed, for next time use abs(amplitude) instead of direct comparison. in audio-manipulator.py.

--- audio-manipulator.py
+++ audio-manipulator.py
@@ -1,5 +1,29 @@
-read_audio = aifc.open(file[,rb])
+import numpy as np
+import aifc
 
-channels, samplewidth, framerate, frames, comptype, compname = aifc.getparams()
+#read_audio = aifc.open("aiff-16.snd","rb")
 
-write_audio = aifc.open(file[,wb])
+#channels, samplewidth, framerate, frames, comptype, compname = read_audio.getparams()
+
+#write_audio = aifc.open("aiff-16.snd","wb")
+#write_audio.setnchannels(channels)
+#write_audio.setsampwidth(samplewidth)
+#write_audio.framerate(framerate)
+#write_audio.setcomptype("NONE", "NONE")
+
+amplitude = np.array([40, -300, 200, -700, 100, -5, 1, -4, 6, -8, 9, -10])
+frequency = np.fft.fft(amplitude)
+
+amplitude = np.fft.ifft([frequency])[0]
+print amplitude
+
+x = 0
+while x < len(amplitude):
+	if amplitude[x] > 128:
+		amplitude[x] -= (amplitude[x] - 128)
+	elif amplitude[x] < -128:
+		amplitude[x] += (amplitude[x] +128)
+	x += 1
+	
+print amplitude.real
+	
