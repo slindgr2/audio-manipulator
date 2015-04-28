9b4a4bf4bd76a9592a6593f50dbb897914affa3c
slindgr2 <slindgr2@nebrwesleyan.edu>
2015-04-28 15:54:47 -0500

    Continued working found error with fft in audio-manipulator.py

--- audio-manipulator.py
+++ audio-manipulator.py
@@ -6,16 +6,11 @@ read_audio = aifc.open("M1F1-mulawC-AFsp.aif","rb")
 
 channels, samplewidth, rate, frames, comptype, compname = read_audio.getparams()
 
-write_audio = aifc.open("M1F1-mulawC-AFsp_output.aif","wb")
-write_audio.setnchannels(channels)
-write_audio.setsampwidth(samplewidth)
-write_audio.setframerate(rate)
-write_audio.setcomptype("NONE", "NONE")
 
 amplitude = np.fromstring(read_audio.readframes(frames), np.int16)
 frequency = np.fft.fft(amplitude)
 
-amplitude = np.fft.ifft([frequency])[0]
+#amplitude = np.fft.ifft([frequency])[0]
 amp_limit = (256**read_audio.getsampwidth()) / 2 - 1
 x = 0
 while x < len(amplitude):
@@ -24,38 +19,52 @@ while x < len(amplitude):
 	elif amplitude[x] < - amp_limit:
 		amplitude[x] -= (amplitude[x] + amp_limit) 
 	x += 1
-
 #averaging
-
 k = 0
 z = 1
-v = 0
-ave_amp = amplitude
+ave_amp = []
+assert read_audio.getnchannels() == 2
 while k < len(amplitude) -1 and z < len(amplitude):
-	ave_amp[v] = (amplitude[k] + amplitude[z])/2
+	ave_amp.append((amplitude[k] + amplitude[z])/2)
 	k += 2
 	z += 2
-	v += 1 
+ 
 	
-plt.plot(list(range(len(ave_amp))), ave_amp, 'ro')
-plt.show()
-	
-
-
-
-
-
-
-
-
-
-
+#plt.plot(list(range(len(ave_amp))), ave_amp, 'ro')
+#plt.show()
+#plt.plot(list(range(len(ave_freq))), ave_freq, 'ro')
+#plt.show()
 #plt.plot(list(range(len(frequency))), frequency, 'ro')
 #plt.show()
 #plt.plot(list(range(len(amplitude))), amplitude, 'ro')
 #plt.show()
-	
-print amplitude.real
-print frequency
-print read_audio.getnframes() 
-print read_audio.getframerate()
+
+ave_freq = np.fft.fft(ave_amp)
+
+#print amplitude.real
+#print frequency
+#print read_audio.getnframes() 
+#print read_audio.getframerate()
+
+
+v = 0
+while v < len(ave_freq):
+	if ave_freq[v] > 200000:
+		ave_freq[v] -= (ave_freq[v] + 200000)
+	elif ave_freq[v] < -200000:
+		ave_freq[v] -= (ave_freq[v] - 200000)
+	v += 1
+#output_amp = np.fft.ifft([ave_freq])[0] / (amp_limit +1)
+
+
+#plt.plot(list(range(len(ave_freq))), ave_freq, 'ro')
+#plt.show()
+
+
+write_audio = aifc.open("M1F1-mulawC-AFsp_output.aif","wb")
+write_audio.setnchannels(1)
+write_audio.setsampwidth(samplewidth)
+write_audio.setframerate(rate)
+write_audio.setcomptype(read_audio.getcomptype(), read_audio.getcomptype())
+write_audio.writeframes(amplitude)
+write_audio.close()
