f636e8e1463e3098f93a671aa2603c084fe0cea8
smason031 <smason@olin132mac13.nebrwesleyan.edu>
2015-04-30 12:01:21 -0500

    Today we were able to succesfully clamp our audio files and get a return file that had an output sound instead of just static although we were able to hear more background noise as opposed to cleaning up background noise. In the process of checking to make sure it works with multiple audio files and cleaning up the code to make it look nice in audio-manipulator.py.

--- audio-manipulator.py
+++ audio-manipulator.py
@@ -1,16 +1,23 @@
 import numpy as np
 import aifc
 import matplotlib.pyplot as plt
+import scipy.fftpack
 
 read_audio = aifc.open("M1F1-mulawC-AFsp.aif","rb")
 
+# Stores audio info in variables
 channels, samplewidth, rate, frames, comptype, compname = read_audio.getparams()
 
-
+# Create a string of amplitudes for every frame.
 amplitude = np.fromstring(read_audio.readframes(frames), np.int16)
-frequency = np.fft.fft(amplitude)
 
-#amplitude = np.fft.ifft([frequency])[0]
+# Amplitudes are derived from our frequency.
+frequency = scipy.fftpack.rfft(amplitude)
+
+amplitude = scipy.fftpack.irfft([frequency])[0].astype(np.int16)
+print amplitude
+
+#This code takes the amplitudes of our original file and clamps the amplitudes to the max amplitude that a speaker with the given sample width could hold.
 amp_limit = (256**read_audio.getsampwidth()) / 2 - 1
 x = 0
 while x < len(amplitude):
@@ -19,7 +26,8 @@ while x < len(amplitude):
 	elif amplitude[x] < - amp_limit:
 		amplitude[x] -= (amplitude[x] + amp_limit) 
 	x += 1
-#averaging
+	
+#averaging, The original file contains two voices in stereo format with n = 2 channels, this code takes the amplitudes of those channels and averages them into 1 mono channel. 
 k = 0
 z = 1
 ave_amp = []
@@ -28,25 +36,24 @@ while k < len(amplitude) -1 and z < len(amplitude):
 	ave_amp.append((amplitude[k] + amplitude[z])/2)
 	k += 2
 	z += 2
- 
-	
-#plt.plot(list(range(len(ave_amp))), ave_amp, 'ro')
-#plt.show()
-#plt.plot(list(range(len(ave_freq))), ave_freq, 'ro')
-#plt.show()
-#plt.plot(list(range(len(frequency))), frequency, 'ro')
-#plt.show()
-#plt.plot(list(range(len(amplitude))), amplitude, 'ro')
-#plt.show()
 
-ave_freq = np.fft.fft(ave_amp)
+ave_amp = np.array(ave_amp)	
+print np.array(ave_amp)
+ave_freq = scipy.fftpack.rfft(ave_amp)
+
+plt.plot(list(range(len(ave_amp))), ave_amp, 'ro')
+plt.show()
+plt.plot(list(range(len(ave_freq))), ave_freq, 'ro')
+plt.show()
+plt.plot(list(range(len(frequency))), frequency, 'ro')
+plt.show()
 
 #print amplitude.real
 #print frequency
 #print read_audio.getnframes() 
 #print read_audio.getframerate()
 
-
+# Clamps our frequency to under 200 kHz
 v = 0
 while v < len(ave_freq):
 	if ave_freq[v] > 200000:
@@ -54,11 +61,37 @@ while v < len(ave_freq):
 	elif ave_freq[v] < -200000:
 		ave_freq[v] -= (ave_freq[v] - 200000)
 	v += 1
-#output_amp = np.fft.ifft([ave_freq])[0] / (amp_limit +1)
 
-
-#plt.plot(list(range(len(ave_freq))), ave_freq, 'ro')
+output_amp = scipy.fftpack.irfft([ave_freq])[0]
+output_amp = np.array(output_amp)
+#print output_amp
+#plt.plot(list(range(len(output_amp))), output_amp, 'ro')
 #plt.show()
+
+
+
+
+
+
+
+
+
+
+
+# if true:
+# 	play female voice
+# elif true 
+# 	play male voice
+# elif true
+# 	play original
+# else
+# 	print please choose an option.
+# 	
+	
+
+
+
+
 
 
 write_audio = aifc.open("M1F1-mulawC-AFsp_output.aif","wb")
@@ -66,5 +99,5 @@ write_audio.setnchannels(1)
 write_audio.setsampwidth(samplewidth)
 write_audio.setframerate(rate)
 write_audio.setcomptype(read_audio.getcomptype(), read_audio.getcomptype())
-write_audio.writeframes(amplitude)
+write_audio.writeframes(ave_amp.astype(np.int16).tostring())
 write_audio.close()
